ef0233c392bd005f89fcbcc1a499fda6f7253bac
minimo-io <nicolas@minimo.io>
2022-05-10 18:13:25 -0300

    Font fixes.

--- uncode-child/shortcodes/beerOnlineStores.inc.php
+++ uncode-child/shortcodes/beerOnlineStores.inc.php
@@ -32,9 +32,12 @@ function hops_beer_online_stores( $atts, $content = "" ) {
                           [vc_column_inner column_width_use_pixel="yes" align_horizontal="align_center" gutter_size="3" overlay_alpha="50" shift_x="0" shift_y="0" shift_y_down="0" z_index="0" medium_width="0" mobile_width="0" width="1/1" column_width_pixel="700"]
                             [vc_custom_heading heading_semantic="h3" text_font="font-762333" text_size="h1" sub_lead="no"]
                               Compra online
+                              <p class="text-center"><i class="fa fa-bell" aria-hidden="true"></i> Recuerda que comprando a través de la app de HOPS ganás puntos canjeables por descuentos en los bares asociados.</p>
                             [/vc_custom_heading]
+
                           [/vc_column_inner]
-                        [/vc_row_inner]');
+                        [/vc_row_inner]
+                        ');
 
     //echo do_shortcode('[vc_empty_space empty_h="2"][vc_separator sep_color=",Default"]');
 
@@ -53,8 +56,8 @@ function hops_beer_online_stores( $atts, $content = "" ) {
                           <img src="https://hops.uy/wp-content/uploads/2022/05/hops-logo-border.png" style="width:50px;min-width:50px;">
                         </td>
                         <td>
-                          [vc_custom_heading heading_semantic="h6" text_size="h2"]
-                              Hops
+                          [vc_custom_heading text_font="font-762333" heading_semantic="h6" text_size="h2"]
+                              HOPS
                           [/vc_custom_heading]
                         </td>
                       </tr>
@@ -64,7 +67,7 @@ function hops_beer_online_stores( $atts, $content = "" ) {
 
                   [/vc_column_inner]
                 [vc_column_inner column_width_percent="100" position_vertical="middle" gutter_size="3" overlay_alpha="50" shift_x="0" shift_y="0" shift_y_down="0" z_index="0" medium_width="2" align_mobile="align_center_mobile" mobile_width="3" width="1/4"]
-                  [vc_custom_heading heading_semantic="h6" text_size="h2"]
+                  [vc_custom_heading heading_semantic="h6" text_font="font-762333" text_size="h2"]
                     '.($product ? '$'.$product->get_price() : "-").'
                   [/vc_custom_heading]
                 [/vc_column_inner]
@@ -77,13 +80,6 @@ function hops_beer_online_stores( $atts, $content = "" ) {
               [/vc_column_inner]
             [/vc_row_inner]
 
-            [vc_row_inner row_inner_height_percent="0" overlay_alpha="50" equal_height="yes" gutter_size="3" shift_y="0" z_index="0"]
-                [vc_column_inner column_width_percent="100" position_vertical="middle" gutter_size="2" overlay_alpha="50" shift_x="0" shift_y="0" shift_y_down="0" z_index="0" medium_width="4" align_mobile="align_right_mobile"]
-                  [vc_empty_space empty_h="1"]
-                    <p class="text-center">Comprando a través de la app de HOPS ganás puntos canjeables por descuentos en los bares asociados.</p>
-                  [vc_empty_space empty_h="1"]
-                [/vc_column_inner]
-            [/vc_row_inner]
 
             [vc_empty_space empty_h="1"][vc_separator sep_color=",Default"][vc_empty_space empty_h="1"]');
     }
@@ -102,11 +98,15 @@ function hops_beer_online_stores( $atts, $content = "" ) {
       $storeIsOffline = get_field("is_offline", $store->ID);
       //var_dump($store);
 
-      $storeMessage = '<i class="fa fa-check-circle" '.($storeIsVerified ? 'style="color:#3BA2F2;' : '' ).'" aria-hidden="true"></i> '.($storeIsVerified ? "Verificada" : "No verificada" );
+      $storeMessage = '<i class="fa '.($storeIsVerified ? 'fa-check-circle' : 'fa-question-circle').'" '.($storeIsVerified ? 'style="color:#3BA2F2;' : '' ).'" aria-hidden="true"></i> '.($storeIsVerified ? "Verificada" : "No verificada" );
       if ($storeIsOffline) $storeMessage = '<i class="fa fa-times-circle" aria-hidden="true"></i> Offline';
 
       $storeButtonBuy = '[vc_button rel="nofollow noreferrer noopener" button_color="color-742106" size="btn-lg" radius="btn-round" border_animation="btn-ripple-out" hover_fx="full-colored" shadow="yes" border_width="0" display="block" link="url:'.urlencode($storeItemUrl).'||target:_blank|" width="140" el_class="hero-download-button hero-message-download-button"]Comprar[/vc_button]';
       if ($storeIsOffline) $storeButtonBuy = "";
+
+
+      $lastUpdateTooltip = ($storesPricesLastUpdate ? '<a class="btn-tooltip" href="#" data-toggle="tooltip" data-placement="top" data-original-title="Actualización: '.$storesPricesLastUpdate.'" title=""><i class="fa fa-info-circle" aria-hidden="true"></i></a>' : '');
+
 
       $ret .= do_shortcode('
 
@@ -119,7 +119,7 @@ function hops_beer_online_stores( $atts, $content = "" ) {
                           <img src="'.$storeImage.'" style="width:50px;min-width:50px;">
                         </td>
                         <td>
-                          [vc_custom_heading heading_semantic="h6" text_size="h4"]
+                          [vc_custom_heading text_font="font-762333" heading_semantic="h6" text_size="h4"]
                               '.hops_offline_text($storeName, $storeIsOffline).'
                           [/vc_custom_heading]
                         </td>
@@ -130,13 +130,13 @@ function hops_beer_online_stores( $atts, $content = "" ) {
 
                   [/vc_column_inner]
                 [vc_column_inner column_width_percent="100" position_vertical="middle" gutter_size="3" overlay_alpha="50" shift_x="0" shift_y="0" shift_y_down="0" z_index="0" medium_width="2" align_mobile="align_center_mobile" mobile_width="3" width="1/4"]
-                  [vc_custom_heading heading_semantic="h6" text_size="h4"]
-                    '.hops_offline_text('$'.$storeItemPrice, $storeIsOffline).'
+                  [vc_custom_heading heading_semantic="h6" text_font="font-762333" text_size="h4"]
+                    '.hops_offline_text('$'.$storeItemPrice.$lastUpdateTooltip, $storeIsOffline).'
                   [/vc_custom_heading]
                 [/vc_column_inner]
 
                 [vc_column_inner mobile_visibility="yes" mobile_width="0" column_width_percent="100" position_vertical="middle" gutter_size="3" overlay_alpha="50" shift_x="0" shift_y="0" shift_y_down="0" z_index="0" medium_width="2" width="1/4"]
-                  [vc_custom_heading heading_semantic="h6" text_size="h4"]
+                  [vc_custom_heading text_font="font-156269" el_class="storeBuyOnlineStatus" heading_semantic="h6" text_size="h4"]
 
                     '.$storeMessage.'
 
@@ -163,7 +163,7 @@ function hops_beer_online_stores( $atts, $content = "" ) {
       $itemKey++;
     }
 
-    $ret .= ($storesPricesLastUpdate ? 'Última actualización: '.$storesPricesLastUpdate : '');
+    // $ret .= ($storesPricesLastUpdate ? 'Última actualización: '.$storesPricesLastUpdate : '');
 
     // no stores, then hide the block
     if ($itemKey == 0){
